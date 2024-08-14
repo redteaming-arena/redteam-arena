394573261fe91cd871bccf66ad4de5ee05d8c350
Redteam Arena <angelopoulos@berkeley.edu>
2024-08-14 05:06:36 +0000

    [get fd]

--- web/src/components/FailurePage.js
+++ web/src/components/FailurePage.js
@@ -4,7 +4,7 @@ import Leaderboard from './Leaderboard';
 const FailurePage = ({ onReset }) => (
   <div className="flex flex-col items-center justify-center min-h-screen bg-black text-red-500 font-mono p-4">
     <h1 className="text-6xl mb-4">You Failed.</h1>
-    <h2 className="text-2xl mb-8">Try and be better next time.</h2>
+    <h2 className="text-2xl mb-8">Get fucked, loser.</h2>
     <button 
       onClick={onReset}
       className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 mb-8"
