74ce244cc610c4c2dff37efc5901949c4e22c515
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2024-09-22 17:01:34 -0700

    [add dev mode] dev mode for CORS. also remove secretKey.py and replace with dotenv

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,15 @@
 # app/core/config.py
 
+import os
 from pydantic_settings import BaseSettings
-from .secretKey import secretKey
+import dotenv
+
+dotenv.load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Red-Teaming Arena"
-    ALLOWED_ORIGINS: list = ["*"]#["https://www.redarena.ai", "https://redarena.ai", "http://redarena.ai"]
-    SECRET_KEY: str = secretKey
+    ALLOWED_ORIGINS: list = ["*"] if (os.environ.get("ENV") == "DEV") else ["https://www.redarena.ai", "https://redarena.ai", "http://redarena.ai"]
+    SECRET_KEY: str = os.environ.get("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 300000
 
