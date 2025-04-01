87dd75fb7ed7bf9b4f2dac2d985658ab67d6e14e
terrianne-zhang <terriannezhang@berkeley.edu>
2025-03-31 23:29:12 -0700

    changes to config.py file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,16 +1,20 @@
 # app/core/config.py
 
-import os
 from pydantic_settings import BaseSettings
+from typing import List
 import dotenv
 
 dotenv.load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Red-Teaming Arena"
-    ALLOWED_ORIGINS: list = ["*"] if (os.environ.get("ENV") == "DEV") else ["https://www.redarena.ai", "https://redarena.ai", "http://redarena.ai"]
-    SECRET_KEY: str = os.environ.get("SECRET_KEY")
+    ALLOWED_ORIGINS: List[str] = ["*"]
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 300000
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 settings = Settings()
